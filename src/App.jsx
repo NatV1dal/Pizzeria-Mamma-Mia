39d7de94047b9ad3a81153b1f4e3839f6c984fd2
natalie vidal <natalie.vidal.a@gmail.com>
2025-01-09 00:48:15 -0300

    translado de componentes a pages

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,10 @@ import { Container } from 'react-bootstrap';
 import NavBar from './components/Navbar';
 //import Home from './pages/Home'
 import Footer from './components/Footer'
-//import Register from './components/Register';
-//import Login from './components/Login';
-//import Cart from './components/Cart';
-import Pizza from './data/Pizza';
+//import Register from './pages/Register';
+//import Login from './pages/Login';
+//import Cart from './pages/Cart';
+import Pizza from './pages/Pizza';
 
 
 function App() {
